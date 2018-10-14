f97f17f38ce4d492065a1f815a8523ba2f60cacf
TheDefrostingSJ <33266672+TheDefrostingSJ@users.noreply.github.com>
2018-10-14 16:02:02 -0500

    final cleanup

--- src/Main/Driver.java
+++ src/Main/Driver.java
@@ -1,11 +1,11 @@
 package Main;
 import java.io.IOException;
+
 import java.util.ArrayList;
 
 import UI.Clickable;
 import UI.Drawable;
 import UI.GameLauncher;
-import UI.TestGUI;
 
 public class Driver {
 
@@ -14,13 +14,9 @@ public class Driver {
 	public static GameLauncher startGame;
 	
 	public static void main(String[] args) throws IOException {
-		boolean testMode = false;
 		
-		if (testMode) {
-			TestGUI testGUI = new TestGUI();
-		} else {
-			startGame = new GameLauncher();
-		}
+	startGame = new GameLauncher();
+	
 	}
 
 }
